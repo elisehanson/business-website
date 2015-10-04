501034057b380dd1e338b0064265d1acd6ac589a
Dawn <2014fed@gmail.com>
2015-10-04 04:46:59 -0400

    draft init

--- main.rb
+++ main.rb
@@ -23,21 +23,3 @@ require 'sinatra'
    erb :contact
   end
 
-
-
-require 'mandrill' 
-
-m = Mandrill::API.new 
-message = {  
-	:subject=> "Hello from the Mandrill API",  
-	:from_name=> "Your name",  
-	:text=>"Hi message, how are you?",  
-	:to=>[{:email=> "email@email.com", 
-	:name=> "Name Name"}],  
-	:html=>"<html><h1>Hi <strong>message</strong>, how are you?</h1></html>",  
-	:from_email=>"email@email.com" 
-} 
-	sending = m.messages.send message 
-	puts sending
-
-
---
+++ public/scripts/gallery.js
@@ -0,0 +1,16 @@
+          
+ $(document).ready(function() {
+            
+            $('#carousel ul').animate({marginLeft:'-600px'},2000,function(){
+                
+                setInterval(function(){
+                $(this).find("li:last").after($(this).find("li:first"));
+                $(this).css({marginLeft:0});
+                
+                });
+            },4000);
+            });
+        
+
+
+
